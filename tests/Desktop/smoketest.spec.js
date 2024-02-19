5621c636a2f44f320e4cbc1d30e8d34196a95347
Neelkonar7 <138477741+Neelkonar7@users.noreply.github.com>
2024-02-19 12:47:20 +0530

    Add files via upload
    Uploaded via Git hub

--- tests/Desktop/smoketest.spec.js
+++ tests/Desktop/smoketest.spec.js
@@ -1,13 +1,14 @@
 //const faker = require('@faker-js/faker@')
 const {test, expect} = require('@playwright/test')
-const login = require('../selectors/login.json')
-const {Login_User} = require('../../pageobject/Account')
+const login = require('../selectors/userauthentication.json')
+const {Authentication} = require('../../pageobject/Account')
+const cart = require('../selectors/cart.json')
 
 const exp = require('constants')
-test.describe("Login",async()=>{
+test.describe("user Authentication",async()=>{
 
     let page
-    test.beforeAll(async({browser})=>{
+    test.beforeEach(async({browser})=>{
     const context = await browser.newContext()
     page = await context.newPage()
     await page.goto("/")
@@ -15,11 +16,19 @@ test.describe("Login",async()=>{
 })
 
     test("Verify Login",async()=>{
-        const Login = new Login_User(page)
+        const Login = new Authentication(page)
         const password = "wxyz@1234"
         await Login.signin(password)
         await Login.logOut()
-    })  
+    }) 
+    
+    test("user Registration",async()=>{
+        const Login = new Authentication(page)
+        await Login.createnewUser()
+        await Login.myAccount()
+        await page.waitForTimeout(3000)
+        await page.getByRole('button', { name: 'Sign Out' }).click()
+    })
 })
 
 
@@ -34,18 +43,17 @@ test.beforeAll(async({browser})=>{
 })
 
 test("Verify Add Product to Cart",async()=>{
-    await page.goto("/pro-tools/pro-irons/express-ion-clipped-detachable-curling-iron")
-    await page.locator("#cartBtn").click()
-    const success_addtocartmsg = await page.locator("#toast-root").textContent()
-    expect(page.locator("#toast-root")).toHaveText("Your product has successfully been added to your bag.")
+    await page.goto(cart.PDP.product_url)
+    await page.locator(cart.cartBtn).click()
+    //const success_addtocartmsg = await page.locator(cart.success_addtocartmsg).textContent()
+    expect(page.locator(cart.success_addtocartmsg)).toHaveText(cart.expected_message)
 })
 
 test("Verify Qty can be increased and add Product to Cart",async()=>{
-    await page.goto("/pro-tools/pro-irons/express-ion-clipped-detachable-curling-iron")
+    await page.goto(cart.PDP.product_url)
     await page.getByRole('textbox',{name:'Item Quantity'}).fill("3")
-    await page.locator("#cartBtn").click()
-    const success_addtocartmsg = await page.locator("#toast-root").textContent()
-    expect(success_addtocartmsg).toContain("Your product has successfully been added to your bag.")
+    await page.locator(cart.cartBtn).click()
+    expect(page.locator(cart.success_addtocartmsg)).toHaveText(cart.expected_message)
     //await page.locator(".item-root-fcS.grid.opacity-100.text-sm")
     await page.waitForTimeout(5000)
     const value = await page.locator(".item-minicartQuantityChanger-5WB").getByRole('textbox',{name:'Item Quantity'})
@@ -54,36 +62,33 @@ test("Verify Qty can be increased and add Product to Cart",async()=>{
 
 
 test("Verify Configurable product to be added to Cart",async()=>{
-    await page.goto("/tea-tree/tea-tree-special-detox/tea-tree-special-detox-kombucha-rinse")
-    const select = page.locator(".tileList-root-7EZ")
+    await page.goto(cart.PDP.config_product_prod)
+    const select = page.locator(cart.product_variants)
     await select.click()
     await select.selectOption({index:2})    
-    await page.locator("#cartBtn").click()
-    const success_addtocartmsg = await page.locator("#toast-root").textContent()
-    expect(page.locator("#toast-root")).toHaveText("Your product has successfully been added to your bag.")
+    await page.locator(cart.cartBtn).click()
+    expect(page.locator(cart.success_addtocartmsg)).toHaveText(cart.expected_message)
 
 })
 
-test("Verify Shopping Cart",async()=>{
-    await page.goto("/tea-tree/tea-tree-special-detox/tea-tree-special-detox-kombucha-rinse")
-    const select = page.locator(".tileList-root-7EZ")
-    await select.click()
-    await select.selectOption({index:2})    
-    await page.locator("#cartBtn").click()
-    const success_addtocartmsg = await page.locator("#toast-root").textContent()
-    expect(page.locator("#toast-root")).toHaveText("Your product has successfully been added to your bag.")
-    await page.locator(".miniCart-sliderLine-Ht4").click()
+test("Verify Checkout process",async()=>{
+
+    await page.locator(cart.minicart_btn).click()
     expect(page.url()).toContain("https://www.paulmitchell.com/cart")
-    const product_price = await page.locator(".product-price-nQ-").first().textContent()
-    const trim_price = product_price.replace(/[^\d.]/g, '');
-    console.log(product_price)
-    console.log(trim_price)
-    const qty_value = await page.getByRole('textbox', { name: 'Item Quantity' }).first().inputValue()
-    const total_price = trim_price * qty_value
-    const subtotal = await page.locator(".priceSummary-price-Nuk.justify-self-end.self-center").first().textContent()
-    const trim_subtotal = subtotal.replace(/[^\d.]/g, '')+"."
-    console.log("Sbutotal: ", trim_subtotal)
-    expect(trim_price).toEqual(trim_subtotal)
+    await page.getByText("Proceed to Checkout").click()
+    expect(page.url()).toContain("https://www.paulmitchell.com/checkout")
+    await page.locator("#email").fill("neelautotest@yopmail.com")
+    await page.locator("#firstname").fill("Neel")
+    await page.locator("#lastname").fill("Autotest")
+    await page.getByPlaceholder("Street Address 1").fill("17802 W Little York Rd")
+    await page.locator("#city").fill("Houston")
+    await page.locator("#region-root-1B2").selectOption("Texas")
+    await page.locator("#postcode-root-ttM").fill("77084")
+    await page.locator("#telephone").fill("9000000000")
+    await page.getByText("Continue to Shipping Method").click()
+    await page.locator(".shippingRadios-radioLabel-pzL").first().click()
+    await page.locator(".shippingMethod-formButtons-C4X.grid").click()
+
 })
 
 
